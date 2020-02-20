94a1f3c588692e642aa78fb99fdb8987d7283ecc
withgod <noname@withgod.jp>
2020-02-21 04:18:56 +0900

    modified

--- app/Http/Controllers/MemcachedController.php
+++ app/Http/Controllers/MemcachedController.php
@@ -25,7 +25,7 @@ class MemcachedController extends Controller
         echo "loop start.\n";
         echo "time:" . microtime(true) . "\n";
         $keys = ['memc-bench-0000000000000'];
-        $expire = time() + 300;
+        $expire = now()->addMinutes(3);
         Cache::put($keys[0], str_repeat('0', 32), $expire);
         $last_time = microtime(true);
         $diffs = [];
@@ -64,6 +64,54 @@ class MemcachedController extends Controller
         echo "\ntime:" . microtime(true) . "\n";
         echo "loop end.\n";
 
+        echo "add method\n";
+        $k = uniqid("memc-bench-add-");
+        $v = str_repeat('x', 32);
+        $ret = Cache::add($k, $v, $expire);
+        echo var_export($ret, true) . "\n";
+        echo "add method failure\n";
+        $ret = Cache::add($k, $v, $expire);
+        echo var_export($ret, true) . "\n";
+        echo "putMany method\n";
+        $putmany = [];
+        $manykeys = [];
+        $k = preg_replace("/add/", "putmany", $k);
+        for ($i = 0; 10 > $i; $i++) {
+            $key = $k . '-' . $i;
+            $manykeys[] = $key;
+            $putmany[$key] = $v;
+        }
+        $ret = Cache::putMany($putmany, $expire);
+        echo var_export($ret, true) . "\n";
+        echo "many method\n";
+        $ret = Cache::many($manykeys);
+        echo var_export($ret, true) . "\n";
+        echo "increment method\n";
+        $k = preg_replace("/putmany/", "increment", $k);
+        $ret = Cache::put($k, 1, $expire);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::increment($k);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::increment($k);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::increment($k , 2);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::get($k);
+        echo var_export($ret, true) . "\n";
+        echo "decrement method\n";
+        $k = preg_replace("/increment/", "decrement", $k);
+        $ret = Cache::put($k, 100, $expire);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::decrement($k);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::decrement($k);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::decrement($k, 2);
+        echo var_export($ret, true) . "\n";
+        $ret = Cache::get($k);
+        echo var_export($ret, true) . "\n";
+
+        echo "*******";
         return response('', 200)->header('Content-Type', 'text/plain');
     }
 }
